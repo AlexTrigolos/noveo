5a5782668e1fc92acc39096c3916830bd4fc884e
Alex Trigolos <alexvlg34@gmail.com>
2022-07-04 21:03:15 +0300

    first home work nothing interesting v2.0

--- first_dz.rb
+++ first_dz.rb
@@ -6,6 +6,13 @@ def sum_arr(arr)
   # arr.sum() # всё можно заменить на эту строку
 end
 
-puts sum_arr(gets.split().map(&:to_f)) # может быть NoMethodError (Method invocation 'split' may produce 'NoMethodError' )
-# на удивление в ruby если не задавать параметры для split, то ему не важно " ", ", ", "; " как минимум)
+our_arr = gets.split(", ") # 1, 2, 3, 4, 5, -12, 143, 3.43, -2.54, 000_000
+# our_arr.each { |elem| puts Float(elem).class == Float }
+begin
+  if our_arr.all?{|elem| Float(elem).class == Float}
+    puts sum_arr(our_arr.map(&:to_f)) # может быть NoMethodError (Method invocation 'split' may produce 'NoMethodError' )
+  end
+rescue
+  puts "Your data is a very big шняга, в общем не только числа :D"
+end
 puts sum_arr([1, 2, 3, 4, 5, -12, 143, 3.43, -2.54, 000_000])
